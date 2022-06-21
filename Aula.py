42cc9a461cd0dee8f7492974fc808bf6a251068b
Edubuenu <107925516+Edubuenu@users.noreply.github.com>
2022-06-21 08:20:28 -0300

    Update Aula.py

--- Aula.py
+++ Aula.py
@@ -1,5 +1,5 @@
 Num1 = int(input('digite o número'))
 Num2 = int(input('digite segundo número'))
 
-Soma = num1 + num2
-Print('O resultado é {}'.format(soma))
+Soma = Num1 + Num2
+Print('O resultado da soma é {}'.format(soma))
